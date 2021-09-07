a95563745d83fb34cc6ba8d412e8af5b32533ddb
Seidmani64 <A01028650@itesm.mx>
2021-09-07 12:35:56 -0500

    Fixed index generation

--- tarea_3/badCharacterRule.h
+++ tarea_3/badCharacterRule.h
@@ -4,33 +4,22 @@ using namespace std;
 
 vector<vector<int>> getIndexes(string pattern, string text)
 {
-    cout<<"Entered idx fun: "<<endl;
     vector<vector<int>> allIndexes;
     vector<int> letterIndexes;
     char currentChar = ' ';
     while(pattern.size() > 0)
     {
-        cout<<"Entered while loop: "<<endl;
         letterIndexes.clear();
         currentChar = pattern.at(pattern.size()-1);
         for(int i = pattern.size()-1; i >= 0; i--)
         {
-            cout<<"Entered for loop with pattern size of: "<<pattern.size()<<endl;
-            if(pattern.size() == 1)
                 if(pattern.at(i)==currentChar)
                 {
-                    cout<<"Pattern before erase: "<<pattern<<endl; 
                     letterIndexes.push_back(i);
-                    pattern.erase(0);
-                    cout<<"Pattern after erase: "<<pattern<<endl; 
-                }
-            else
-                if(pattern.at(i)==currentChar)
-                {
-                    cout<<"Pattern before erase: "<<pattern<<endl; 
-                    letterIndexes.push_back(i);
-                    pattern.erase(i,i);
-                    cout<<"Pattern after erase: "<<pattern<<endl; 
+                    if(pattern.size() == 1)
+                        pattern.erase(0);
+                    else
+                        pattern.erase(i,i);
                 }
         }
         allIndexes.push_back(letterIndexes);
--- tarea_3/stringMatch.cpp
+++ tarea_3/stringMatch.cpp
@@ -7,14 +7,12 @@ using namespace std;
 int main(){
     string text = "THIS IS A SIMPLE EXAMPLE. DO YOU LIKE THIS EXAMPLE?";
     string pattern = "EXAMPLE";
-    cout<<"Before idx call"<<endl;
     vector<vector<int>> allIdx = getIndexes(pattern,text);
-    cout<<"after idx call"<<endl;
     for(int i = 0; i < allIdx.size(); i++)
     {
         cout<<"The value of allIdx at: "<<i<<" is: ";
         for(int j = 0; j < allIdx[i].size(); j++)
-            cout<<allIdx[i][j];
+            cout<<allIdx[i][j]<<" ";
         cout<<endl;
     }
     return 0;
